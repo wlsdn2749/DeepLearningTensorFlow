2129d86ba7d15e0d8615a4b2f0da69642c75ad35
Jinwoo Jung <wlsdn2749@naver.com>
2022-03-04 14:40:03 +0900

    multi_variable_linear_regression add with keras

---
+++ Example/multi_variable_linear_regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+x_data = [[73., 80., 75.],
+          [93., 88., 93.],
+          [89., 91., 90.],
+          [96., 98., 100.],
+          [73., 66., 70.]]
+y_data = [[152.],
+          [185.],
+          [180.],
+          [196.],
+          [142.]]
+
+tf.model = tf.keras.Sequential()
+
+tf.model.add(tf.keras.layers.Dense(units=1, input_dim=3))  # input_dim=3 gives multi-variable regression
+tf.model.add(tf.keras.layers.Activation('linear'))  # this line can be omitted, as linear activation is default
+# advanced reading https://towardsdatascience.com/activation-functions-neural-networks-1cbd9f8d91d6
+
+tf.model.compile(loss='mse', optimizer=tf.keras.optimizers.SGD(lr=1e-5))
+tf.model.summary()
+history = tf.model.fit(x_data, y_data, epochs=100)
+
+y_predict = tf.model.predict(np.array([[73., 80., 75.]]))
+print(y_predict)
